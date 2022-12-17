a7f9f6d9fa354c06714c460fce4a78ad66bf3b0c
Brian Sea <brian.sea@ncssm.edu>
2022-12-17 15:37:58 -0500

    Day 16 Part 1: Speedup be precalculating all the shortest paths

--- 2022/day16/part1.py
+++ 2022/day16/part1.py
@@ -34,20 +34,17 @@ def djikstra(tunnels, start, end):
 
     return path, paths[end][0]
 
-
-# Brute force (BLAH!!!)
-# Need to switch to DP
-def doBranch(tunnels, minutes, released, start, opened):
+def doBranch(tunnels, minutes, released, start, opened, paths):
     maxReleased = released
     if minutes > 0:
         for end in tunnels:
             if tunnels[end]['rate'] > 0 and end not in opened:
-                path, dist = djikstra(tunnels, start, end)
+                path, dist = paths[start][end]
                 
                 if len(path) > 0 and minutes > len(path)+1:                  
                     released += tunnels[end]['rate'] * (minutes-len(path)-1)
                     opened.add(end)            
-                    rtn = doBranch( tunnels, minutes-len(path)-1, released, end, opened)
+                    rtn = doBranch( tunnels, minutes-len(path)-1, released, end, opened, paths)
                     released -= tunnels[end]['rate'] * (minutes-len(path)-1)
                     opened.remove(end)
 
@@ -56,7 +53,7 @@ def doBranch(tunnels, minutes, released, start, opened):
 
     return maxReleased
 
-f = open('input')
+f = open('inputfinal')
 tunnels = {}
 for line in f:  
     label, rate, paths = re.findall("Valve\s([A-Z]+).+=(\d+).+valves?\s([^\n]+)", line)[0]    
@@ -66,13 +63,20 @@ for line in f:
         'leadsTo': list(map(str.strip, paths.split(',')))
     }
 
+# Pre-calculate all the shortest paths
+shortestPaths = {}
+for start in tunnels:
+    to = {}
+    for end in tunnels:  
+        to[end] = djikstra(tunnels, start, end)
+    shortestPaths[start] = to
+
 opened = set()
 minutes = 30
 released = 0
 start = 'AA'
 opened.add(start)
-
-print(doBranch(tunnels, minutes, 0, "AA", opened))
+print(doBranch(tunnels, minutes, 0, "AA", opened,shortestPaths))
 
 # Guess: 1372 (too low)
 # Guess: 1651 (too low)
