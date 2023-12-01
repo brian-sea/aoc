fad5ee55da5eba54dee941857989fab145474323
Brian Sea <brian.sea@gmail.com>
2023-12-01 00:59:17 -0500

    Day 1 Solutions

--- 2023/day01/part1.py
+++ 2023/day01/part1.py
@@ -1,3 +1,26 @@
-f = open("input")
+import re
+f = open("finalinput")
 
+s = 0
 lines = f.readlines()
+for line in lines:
+    first = None
+    last = None
+
+    idx = 0
+    while idx < len(line):
+        if( '0' <= line[idx] <= '9'):
+            first = line[idx]
+            break
+        idx += 1
+
+    idx = len(line) - 1
+    while idx >= 0:
+        if( '0' <= line[idx] <= '9'):
+            last = line[idx]
+            break
+        idx -= 1
+
+    s += int( first + last )
+
+print(s)
--- 2023/day01/part2.py
+++ 2023/day01/part2.py
@@ -1,3 +1,48 @@
 f = open("finalinput")
 
+spelled = {
+    '0' : '0',
+    '1' : '1',
+    '2' : '2', 
+    '3' : '3', 
+    '4' : '4', 
+    '5' : '5', 
+    '6' : '6',
+    '7' : '7',
+    '8' : '8', 
+    '9' : '9',
+    "zero" : '0',
+    "one" : '1',
+    "two" : '2',
+    "three" : '3',
+    "four" : '4',
+    "five" : '5',
+    "six" : '6',
+    "seven" : '7',
+    "eight" : '8',
+    "nine" : '9'
+}
+
+s = 0
 lines = f.readlines()
+for line in lines:
+    first = None
+    last = None
+
+    firstFoundAt = len(line)
+    lastFoundAt = -1
+
+    for k in spelled.keys():
+        f = line.find(k)
+        l = line.rfind(k)
+
+        if f != -1 and f < firstFoundAt:
+            first = spelled[k]
+            firstFoundAt = f
+        if l > lastFoundAt:
+            last = spelled[k]
+            lastFoundAt = l
+
+    s += int( first + last )
+
+print(s)
