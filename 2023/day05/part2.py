f326e8522801142da2983e4a9496d3504ed81a57
Brian Sea <brian.sea@gmail.com>
2023-12-05 22:14:33 -0500

    Day 5 - optimized without brute force

--- 2023/day05/part2.py
+++ 2023/day05/part2.py
@@ -1,11 +1,11 @@
+# Brute Force... ewww; find a better way
+
 import re
 import datetime
-import sys
 
 f = open("finalinput")
 
 lines = [line.strip() for line in f.readlines()]
-s = 0
 
 #start = datetime.datetime.now()
 seedRanges = [int(x) for x in re.findall("\d+", lines[0])]
@@ -13,6 +13,7 @@ seedRanges = [int(x) for x in re.findall("\d+", lines[0])]
 sIdx = 0
 seeds = []
 while sIdx < len(seedRanges):
+    # Intervals -- [include, exclude)
     seeds.append([seedRanges[sIdx], seedRanges[sIdx] + seedRanges[sIdx+1]])
     sIdx += 2
 
@@ -23,7 +24,7 @@ maps = [
     list(), # water-to-light
     list(), # light-to-temp
     list(), # temp-to-humidity
-    list()  # humidity-to-location
+    list() # humidity-to-location
 ]
 
 onMap = 0
@@ -38,33 +39,63 @@ while lineCount < len(lines):
     nums = [int(x) for x in re.findall("\d+", lines[lineCount])]
     
     [d,s,r] = nums
-    MAP.append([s,d,r])
+    MAP.append([d,s,r])
 
     lineCount += 1
 
-maps = maps[::-1]
 
-location = 0
-while True:
+minLoc = float('inf')
+for s in seeds:
     
-    loc = location
+    NM = [ [s[0], s[1]] ]
     for m in maps:
-        for l in m:
-            if l[1] <= loc <= l[1]+l[2]:
-                loc = l[0]+(loc-l[1])
-                break   
+        
+        # Seeds adjusted for next map
+        AdjustedSeeds = []
+        for r in m:
 
-    # Check Seeds
-    for l in seeds:
-        if l[0] <= loc <= l[1]:
-            print(location)
-            sys.exit(0)
+            # Storage of unadjusted elements
+            nextMap = []
+            for nm in NM:
+                before = None
+                after = None
+                overlap = None
+            
+                adjustInterval = [nm[0], nm[1]]
+    
+                # before - untouched
+                if adjustInterval[0] < r[1]:
+                    before = [ adjustInterval[0], min(adjustInterval[1], r[1])]
+                    adjustInterval[0] = r[1]
+                    nextMap.append(before)
+                
+                # after - untouched
+                if adjustInterval[1] > r[1] + r[2]:
+                    after = [ max(adjustInterval[0], r[1]+r[2]), adjustInterval[1] ] 
+                    adjustInterval[1] = r[1]+r[2]
+                    nextMap.append(after)
+                
+                # overlap - adjusted
+                if( adjustInterval[1] - adjustInterval[0] > 0):
+                    adjustedStart = r[0] + (adjustInterval[0]-r[1])
+                    adjustedEnd = r[0] + (adjustInterval[1]-r[1]) 
+                    overlap = [ adjustedStart, adjustedEnd ]
+                    AdjustedSeeds.append(overlap)
+                
+            NM = nextMap
 
-    location += 1
+        # Add adjusted seeds for the next map
+        for a in AdjustedSeeds:
+            NM.append(a)  
 
-#endTime = datetime.datetime.now()
+    # Check current seed interval for min
+    for s in NM:
+        if s[0] < minLoc:
+            minLoc = s[0]
 
+#end = datetime.datetime.now()
 #print(end - start)
 
-# Guess: 52210645 (too high)
+print(minLoc)
+
 # Answer: 52210644
