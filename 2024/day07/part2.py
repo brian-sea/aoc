86c8cbc7be6e12d0f4a5b70084aaa8b8c0438c4d
Brian Sea <brian.sea@gmail.com>
2024-12-07 14:27:19 -0500

    Day 07

---
+++ 2024/day07/part2.py
@@ -0,0 +1,37 @@
+import sys
+import re
+
+filename = sys.argv[1] if len(sys.argv) > 1 else 'test-input.dat'
+f = open(filename)
+
+s = 0
+for line in f:
+    testValue, values = line.strip().split(":")
+    testValue = int(testValue)
+    values = list(map(int, values.split()))
+    
+    qCalc = set()
+    checkCalc = set()
+
+    qCalc.add(values[0])
+    idx = 1
+    while idx < len(values):
+        for val in qCalc:
+
+            calcs = [
+                val + values[idx],
+                val * values[idx],
+                int(str(val)+str(values[idx]))
+            ]
+            for calc in calcs:
+                if calc <= testValue:
+                    checkCalc.add(calc)
+
+        qCalc = checkCalc
+        checkCalc = set()
+        idx += 1
+
+    if testValue in qCalc:
+        s += testValue
+    
+print(s)
